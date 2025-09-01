448007c7430cc62ee22bb67170fa6c385d0d9395
Li Blissz <liblissz3@gmail.com>
2025-08-31 19:27:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# manfess-mobile2
---
+++ index.js
@@ -0,0 +1,10 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriatel https://cli.github.com/?utm_source=chatgpt.com
+registerRootComponent(App);
+//
+
