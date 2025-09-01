0318e9cc1c04ff5c69bb83c909dff3ccddb5b54d
Li Blissz <liblissz3@gmail.com>
2025-09-01 01:50:25 -0700

    Update index.js and .gitignore

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriatel https://cli.github.com/?utm_source=chatgpt.com
 registerRootComponent(App);
-//
+// rm -rf .git     git pull origin main --allow-unrelated-histories
+//llk
 
+
+// git remote add origin git@github.com:vildashnetwork/manfess-mobile2.git
